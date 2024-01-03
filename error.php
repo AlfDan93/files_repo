76836104d62785c5f17393cfd118cb46f9f284b6
AlfDan93 <67742439+AlfDan93@users.noreply.github.com>
2024-01-03 16:54:12 +0100

    Create error.php

---
+++ error.php
@@ -0,0 +1,7 @@
+<html>
+<center><h1>Welcome To Nginx on Kubernetes</h1></center>
+</br>
+<center><img src="https://pbs.twimg.com/profile_images/861570304920997888/7ii88n5J_400x400.>
+</br>
+<center><h1>I AM THE BACKEND ON "<?php echo 'TEST'''' >"</h1></center>
+</html>
